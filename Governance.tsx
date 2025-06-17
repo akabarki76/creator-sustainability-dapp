cd9843b6eca8ab799cde6c7faa1d363b65968969
Mustapha BARKI <code4yo@gmail.com>
2025-06-17 18:17:16 +0200

    Create Governance.tsx

---
+++ Governance.tsx
@@ -0,0 +1,11 @@
+export default function Governance() {
+  return (
+    <div>
+      <h2>Protocol Governance</h2>
+      <button onClick={() => console.log("Navigate to proposal form")}>
+        Submit Proposal
+      </button>
+      {/* List of proposals with vote buttons */}
+    </div>
+  );
+}
