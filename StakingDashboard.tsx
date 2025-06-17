e4d56e4fd260136e09221427b212926cbdfdf1f3
Mustapha BARKI <code4yo@gmail.com>
2025-06-17 18:16:45 +0200

    Create StakingDashboard.tsx

---
+++ StakingDashboard.tsx
@@ -0,0 +1,13 @@
+export default function StakingDashboard() {
+  // Fetch from smart contract: totalStaked, topSupporters
+  return (
+    <div>
+      <h2>Staking Activity</h2>
+      <p>Total Staked: ...</p>
+      <ul>
+        <li>Supporter A - 100 tokens</li>
+        <li>Supporter B - 85 tokens</li>
+      </ul>
+    </div>
+  );
+}
